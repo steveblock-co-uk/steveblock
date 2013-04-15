4b2b41175dc5285b8c18673471fcc382f568099e
Steve Block <steveblock@chromium.org>
2013-04-15 23:07:43 +1000

    Remove a load of unused stuff

--- scripts/utils.js
+++ scripts/utils.js
@@ -7,20 +7,6 @@ Size.prototype.AspectRatio = function() {
   return this.width / this.height;
 }
 
-Size.prototype.Print = function() {
-  return '( ' + this.width + ', ' + this.height + ' )';
-}
-
-function StripQuotes( s ) {
-  var len = s.length;
-  var c1 = s.charAt(0);
-  var c2 = s.charAt(len-1);
-  if( ( c1 == "'" && c2 == "'" ) || ( c1 == '"' && c2 == '"' ) ) {
-    return s.substring( 1, len-1 );
-  }
-  return s;
-}
-
 // Function to fit an object to a given box
 function FitToBoundingBox(box, objectAR) {
   var boxAR    = box.AspectRatio();
@@ -31,14 +17,4 @@ function FitToBoundingBox(box, objectAR) {
     resized = new Size( box.height * objectAR, box.height );
   }
   return resized;
-}
-
-function GetWindowSize() {
-  var width  = document.documentElement.clientWidth;
-  var height = document.documentElement.clientHeight;
-  var scrollbarThickness = 17;
-  if( window.scrollMaxX > 0 ) { height-=scrollbarThickness; }
-  if( window.scrollMaxY > 0 ) { width-=scrollbarThickness; }
-  var size = new Size( width, height );
-  return size;
 }
